ec31c8c51bdab1babb45f38d508dcefd15bf56d3
elizianeandreia <liziandreia@gmail.com>
2025-01-30 15:29:58 -0300

    corrigindo pequenos bugs

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 var entrarBtn = document.getElementById("entrarBtn");
-
 var formPage = document.getElementById("formPage");
 var idadeForm = document.getElementById("idadeForm");
 var limparBtn = document.getElementById("limparBtn");
 
-
 entrarBtn.addEventListener("click", function () {
-  formPage.style.display = "block";
-  formPage.classList.add('fadeIn'); 
-});
+    formPage.style.display = "block";
+    formPage.classList.add('fadeIn');
+  });
 
 
-idadeForm.addEventListener("submit", function (e) {
-  e.preventDefault();
+  idadeForm.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  var idadeInput = document.getElementById("idade");
-  var idade = idadeInput.value;
+    var idadeInput = document.getElementById("idade");
+    var idade = idadeInput.value;
 
-  if (idade) {
-    alert("Obrigado por preencher a idade!");
-  } else {
-    alert("Por favor, preencha a sua idade.");
-  }
-);
+    if (idade) {
+        alert("Obrigado por preencher a idade!");
+      } else {
+        alert("Por favor, preencha a sua idade.");
+      }
+    });
 
 
 
-limparBtn.addEventListener("click", function () {
-
-  var idadeInput = document.getElementById("idade");
-  idadeInput.value = ''; 
-  formPage.style.display = "none"; 
-});
+    limparBtn.addEventListener("click", function () {
+        var idadeInput = document.getElementById("idade");
+        idadeInput.value = ''; // Limpa o campo de idade
+        formPage.style.display = "none"; // Opcional: Esconde o formulário novamente
+      });
